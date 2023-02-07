1652269fb9386f875494dbe630dfe791d0ada112
Youssef El Khattabe <kharbouchitv@gmail.com>
2023-02-07 17:59:59 +0100

    Day 13

--- bot.py
+++ bot.py
@@ -2,8 +2,9 @@ import random
 import subprocess
 with open("changes.txt","a+") as f :
     for i in range(1,365) :
+        f.write(f'Commited Date Number : {i}')
         if random.randint(0,9) > 8 :
-            f.write(f'Commited Dat Number : {i}')
+            
             commit_message = f"Day {i}"
             subprocess.call(['git', 'add', '.'])
             subprocess.call(['git', 'commit', '--date',f"{i} days ago",'-m', commit_message])
